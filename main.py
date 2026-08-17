d910f76cc5491f33e26769f91e2d8b986624bf8a
Mahmoud Ahmed <MahmoudAhmedMahmoudAttia@gmail.com>
2026-08-17 08:37:23 +0000

    update main

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-from routes.base import base_router
-
-load_dotenv()
+from routes import base, data
 
 app = FastAPI()
 
-app.include_router(base_router, prefix="/api/v1", tags=["api_v1"])
+app.include_router(base.base_router)
+app.include_router(data.data_router)
