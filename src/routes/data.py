2de3acb0fc5def7c5c952c93aac86bac41daaa46
Mahmoud Ahmed <MahmoudAhmedMahmoudAttia@gmail.com>
2026-08-21 05:23:45 +0000

    update routes data

--- src/routes/data.py
+++ src/routes/data.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.config import get_settings, Settings
-from controllers import DataController, ProjectController
-from models import ResponseSignal
+from controllers import DataController, ProjectController, ProcessController
 import aiofiles
+from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -26,6 +27,7 @@ async def upload_data(project_id: str, file: UploadFile,
             content={"signal": result_signal}
         )
 
+    project_dir_path = ProjectController().get_project_path(project_id=project_id)
     file_path, file_id = data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
@@ -48,3 +50,35 @@ async def upload_data(project_id: str, file: UploadFile,
             "file_id": file_id
         }
     )
+
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id: str, process_request: ProcessRequest):
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id=project_id)
+
+    file_content = process_controller.get_file_content(file_id=file_id)
+
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        file_id=file_id,
+        chunk_size=chunk_size,
+        overlap_size=overlap_size
+    )
+
+    if file_chunks is None or len(file_chunks) == 0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"signal": ResponseSignal.PROCESSING_FAILED.value}
+        )
+
+    return JSONResponse(
+        content={
+            "signal": ResponseSignal.PROCESSING_SUCCESS.value,
+            "chunks_count": len(file_chunks),
+            "chunks": [chunk.page_content for chunk in file_chunks]
+        }
+    )
+ 
