500694ac4ac7df7f891dc6fcaa3eedf03afd0f06
Mahmoud Ahmed <MahmoudAhmedMahmoudAttia@gmail.com>
2026-08-29 07:59:39 +0000

    tutorial-5: DB schemas

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,5 +1,5 @@
+from pydantic_settings import BaseSettings
 
-from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
@@ -7,7 +7,11 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    MONGODB_URL: str
+    MONGODB_DATABASE: str
+
     class Config:
         env_file = ".env"
+
 def get_settings():
     return Settings()
--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
 import sys
 import os
-
 sys.path.insert(0, os.path.dirname(__file__))
 
 from fastapi import FastAPI
 from routes import base, data
+from stores.mongodb import connect_to_mongo, close_mongo_connection
 
 app = FastAPI()
 
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
- 
