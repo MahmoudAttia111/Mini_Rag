5a9bb1cb7dd786c4acd13ea399b46938dff79da7
Mahmoud Ahmed <MahmoudAhmedMahmoudAttia@gmail.com>
2026-09-02 11:47:44 +0000

    add asset model

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -1,4 +1,5 @@
 from enum import Enum
+
 class ResponseSignal(Enum):
     FILE_VALIDATED_SUCCESS = "file_validate_successfully"
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
@@ -7,5 +8,5 @@ class ResponseSignal(Enum):
     FILE_UPLOAD_FAILED = "file_upload_failed"
     PROCESSING_SUCCESS = "processing_success"
     PROCESSING_FAILED = "processing_failed"
-
-    
+    NO_FILES_ERROR = "not_found_files"
+    FILE_ID_ERROR = "no_file_found_with_this_id"
