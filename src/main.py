44b7148341c065436264ef9d198d5cad99b15710
Mahmoud Ahmed <MahmoudAhmedMahmoudAttia@gmail.com>
2026-09-08 11:11:57 +0000

    tutorial-10=: NLP controller + vector DB search

--- src/main.py
+++ src/main.py
@@ -2,38 +2,40 @@ import sys, os
 sys.path.insert(0, os.path.dirname(__file__))
 
 from fastapi import FastAPI
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
-from contextlib import asynccontextmanager
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
+app = FastAPI()
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # ---- Startup ----
+@app.on_event("startup")
+async def startup_db_client():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
-    llm_provider_factory = LLMProviderFactory(settings)
+    llm_factory = LLMProviderFactory(settings)
+    vectordb_factory = VectorDBProviderFactory(settings)
 
-    # generation client
-    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client = llm_factory.create(provider=settings.GENERATION_BACKEND)
     app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
-    # embedding client
-    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
-    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
-                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+    app.embedding_client = llm_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID,
+        embedding_size=settings.EMBEDDING_MODEL_SIZE
+    )
 
-    yield
+    app.vectordb_client = vectordb_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
 
-    # ---- Shutdown ----
+@app.on_event("shutdown")
+async def shutdown_db_client():
     app.mongo_conn.close()
-
-
-app = FastAPI(lifespan=lifespan)
+    app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
